c157ce569ad68a30adba5ea2685d85b7ff82fcd3
Trochez <juan.trochez95@gmail.com>
2016-11-25 13:49:51 +0100

    [film] image
    add image display 
    video lecteur in film view

--- app/controllers/films_controller.rb
+++ app/controllers/films_controller.rb
@@ -10,6 +10,7 @@ class FilmsController < ApplicationController
   # GET /films/1
   # GET /films/1.json
   def show
+    @videos = Video.where(:film_id => @film.id)
   end
 
   # GET /films/new
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   resources :videos
   resources :genres
   resources :films
-  root 'home#index'
+  root 'films#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
