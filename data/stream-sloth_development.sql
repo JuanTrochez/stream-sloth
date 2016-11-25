430bfb799e60e3e187366242d0beee170fd8e2d8
Trochez <juan.trochez95@gmail.com>
2016-11-25 14:27:26 +0100

    [bdd|css]
    new bdd
    css home

--- data/stream-sloth_development.sql
+++ data/stream-sloth_development.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Jeu 24 Novembre 2016 à 14:24
+-- Généré le :  Ven 25 Novembre 2016 à 13:25
 -- Version du serveur :  5.7.11
 -- Version de PHP :  5.6.18
 
@@ -38,7 +38,7 @@ CREATE TABLE `ar_internal_metadata` (
 --
 
 INSERT INTO `ar_internal_metadata` (`key`, `value`, `created_at`, `updated_at`) VALUES
-('environment', 'development', '2016-11-24 14:08:20', '2016-11-24 14:08:20');
+('environment', 'development', '2016-11-24 18:16:15', '2016-11-24 18:16:15');
 
 -- --------------------------------------------------------
 
@@ -54,12 +54,28 @@ CREATE TABLE `films` (
   `date_production` date DEFAULT NULL,
   `synopsis` text,
   `pays` varchar(255) DEFAULT NULL,
-  `genres` varchar(255) DEFAULT NULL,
+  `genre_id` int(11) DEFAULT NULL,
   `duree` varchar(255) DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
   `created_at` datetime NOT NULL,
-  `updated_at` datetime NOT NULL
+  `updated_at` datetime NOT NULL,
+  `image_file_name` varchar(255) DEFAULT NULL,
+  `image_content_type` varchar(255) DEFAULT NULL,
+  `image_file_size` int(11) DEFAULT NULL,
+  `image_updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `films`
+--
+
+INSERT INTO `films` (`id`, `titre`, `realisateur`, `date_sortie`, `date_production`, `synopsis`, `pays`, `genre_id`, `duree`, `image`, `created_at`, `updated_at`, `image_file_name`, `image_content_type`, `image_file_size`, `image_updated_at`) VALUES
+(1, 'fdsjl', 'jkl', '2016-02-24', '2016-11-24', 'jfl', 'kfjkl', 2, 'fjkl', 'clock_91890.jpg', '2016-11-24 18:50:15', '2016-11-25 10:51:52', NULL, NULL, NULL, NULL),
+(2, 'fjd', 'mfjkdlm', '2016-11-24', '2016-11-24', 'jfkdl', 'fjkl', 1, 'fjklm', 'fed', '2016-11-24 22:07:20', '2016-11-24 22:07:20', NULL, NULL, NULL, NULL),
+(3, 'jlkjfdsl', 'jkflmd', '2016-11-24', '2016-11-24', 'fjkld', 'fjkl', 1, 'fjklf', 'jklmfd', '2016-11-24 22:12:41', '2016-11-24 22:12:41', NULL, NULL, NULL, NULL),
+(4, 'fdjskl', 'fjklm', '2016-02-24', '2016-11-24', 'fjkdl', 'fjkl', 2, 'fjkl', 'fjkl', '2016-11-24 22:28:07', '2016-11-24 22:28:20', NULL, NULL, NULL, NULL),
+(5, 'Test', 'JKFDLMS', '2016-11-24', '2016-11-24', 'jkflmd', 'fjklm', 2, 'fjklm', 'fjklm', '2016-11-24 22:36:56', '2016-11-24 22:36:56', NULL, NULL, NULL, NULL),
+(6, 'fdj', 'hf', '2016-11-25', '2016-11-25', 'fjdklm', 'fjlk', 1, 'fjdlk', 'alien-head_318-85913.png', '2016-11-25 10:31:48', '2016-11-25 10:31:48', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -73,6 +89,14 @@ CREATE TABLE `genres` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `genres`
+--
+
+INSERT INTO `genres` (`id`, `nom`, `created_at`, `updated_at`) VALUES
+(1, 'Action', '2016-11-24 18:22:37', '2016-11-24 18:22:37'),
+(2, 'Science - Fiction', '2016-11-24 18:22:51', '2016-11-24 18:22:51');
 
 -- --------------------------------------------------------
 
@@ -91,7 +115,8 @@ CREATE TABLE `schema_migrations` (
 INSERT INTO `schema_migrations` (`version`) VALUES
 ('20161124140612'),
 ('20161124140653'),
-('20161124140723');
+('20161124140723'),
+('20161125093343');
 
 -- --------------------------------------------------------
 
@@ -106,6 +131,13 @@ CREATE TABLE `videos` (
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `videos`
+--
+
+INSERT INTO `videos` (`id`, `lien`, `film_id`, `created_at`, `updated_at`) VALUES
+(1, 'https://www.youtube.com/embed/JwQZQygg3Lk', 1, '2016-11-25 10:41:47', '2016-11-25 10:41:47');
 
 --
 -- Index pour les tables exportées
@@ -149,17 +181,17 @@ ALTER TABLE `videos`
 -- AUTO_INCREMENT pour la table `films`
 --
 ALTER TABLE `films`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT pour la table `genres`
 --
 ALTER TABLE `genres`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `videos`
 --
 ALTER TABLE `videos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
